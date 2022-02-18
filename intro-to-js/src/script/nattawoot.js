38d9759924a4f706b9d30e8778ea653e62ff69c9
nattawoot denduangchai <nattawoot.denduangchai@gmail.com>
2022-02-18 13:44:11 +0700

    js age assignment

--- intro-to-js/src/script/nattawoot.js
+++ intro-to-js/src/script/nattawoot.js
@@ -10,3 +10,19 @@ function helloWorld() {
     const h1message =  document.getElementById("yourName").innerHTML;
     alert(h1message + 'โหลด เตี้ย เพราะ เมีย หนัก')
 }
+
+function calAge() {
+    const birthYear = prompt("When is your birth year?");
+    const numBirthYear = Number(birthYear);
+    const text = "Congrat, you have been here in this country for";
+    const age = 2022 - numBirthYear;
+    alert(`${text} ${age}` + " years");
+}
+
+function loadProfile() {
+    const name "Nattawoot";
+    const province = "Ratchaburi";
+
+    document.getElementById('my-name').innerHTML = name;
+    document.getElementById('my-province').innerHTML = province;
+}
